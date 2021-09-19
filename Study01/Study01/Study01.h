bde8bdb6f3a1d3bed402f97ae73d07035e26c7bf
ChangNam An <ccn0324@gmail.com>
2021-09-19 15:07:21 +0900

    Test Upload

---
+++ Study01/Study01/Study01.h
@@ -0,0 +1,32 @@
+﻿
+// Study01.h: PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CStudy01App:
+// 이 클래스의 구현에 대해서는 Study01.cpp을(를) 참조하세요.
+//
+
+class CStudy01App : public CWinApp
+{
+public:
+	CStudy01App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CStudy01App theApp;
